1d0921942e663e0e9b677ed6730561c586c245e5
Reid <Reid1834@gmail.com>
2018-03-22 08:27:21 +0800

    update 20180322

--- app/src/main/cpp/changeVoice.cpp
+++ app/src/main/cpp/changeVoice.cpp
@@ -13,7 +13,7 @@ using namespace FMOD;
 #define MODE_LUOLI 1
 #define MODE_DASHU 2
 #define MODE_JINGSONG 3
-#define MODE_GAOGUAI
+#define MODE_GAOGUAI 4
 #define MODE_KONGLING 5
 
 #define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"axe",FORMAT,##__VA_ARGS__);
@@ -22,7 +22,7 @@ extern "C"
 
 JNICALL
 JNIEXPORT void JNICALL Java_com_reid_qqvoice_VoiceTools_changeVoice
-(JNIEnv * jniEnv, jclass jclass1, jstring jstring1, jint mode) {
+(JNIEnv * jniEnv, jclass, jstring jstring1, jint mode) {
     //初始化fmod
     FMOD::System *system;
     FMOD::System_Create(&system);
@@ -40,4 +40,84 @@ JNIEXPORT void JNICALL Java_com_reid_qqvoice_VoiceTools_changeVoice
     //将jstring转为char
     const char *path = jniEnv->GetStringUTFChars(jstring1, NULL);
     system->createSound(path, FMOD_DEFAULT, NULL, &sound);
+
+    try {
+        switch (mode) {
+            case MODE_NORMAL:
+                LOGI("%s", "正常");
+                system->playSound(sound, NULL, false, &channel);
+                break;
+            case MODE_DASHU:
+                // 设置音调，调低音调
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pDSP);
+                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 0.7);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
+                break;
+            case MODE_LUOLI:
+                LOGI("%s", "萝莉");
+                // 设置音调，调高音调
+                system->createDSPByType(FMOD_DSP_TYPE_PITCHSHIFT, &pDSP);
+                pDSP->setParameterFloat(FMOD_DSP_PITCHSHIFT_PITCH, 3);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
+                break;
+            case MODE_GAOGUAI:
+                LOGI("%s", "搞怪");
+                system->createDSPByType(FMOD_DSP_TYPE_NORMALIZE, &pDSP);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
+                // 获取速度并加速
+                channel->getFrequency(&frequency);
+                frequency = frequency * 1.6;
+                channel->setFrequency(frequency);
+                break;
+            case MODE_JINGSONG:
+                // FMOD_DSP_TYPE_TREMOLO 颤抖
+                LOGI("%s", "惊悚");
+                // 设置颤抖
+                system->createDSPByType(FMOD_DSP_TYPE_TREMOLO, &pDSP);
+                // 设置颤抖的频率
+                pDSP->setParameterFloat(FMOD_DSP_TREMOLO_SKEW, 0.8);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
+                channel->getFrequency(&frequency);
+                frequency = frequency * 0.5;
+                channel->setFrequency(frequency);
+                break;
+            case MODE_KONGLING:
+                LOGI("%s", "空灵");
+                // 设置重复
+                system->createDSPByType(FMOD_DSP_TYPE_ECHO, &pDSP);
+                // 设置重复的重复延迟
+                pDSP->setParameterFloat(FMOD_DSP_ECHO_DELAY, 300);
+                pDSP->setParameterFloat(FMOD_DSP_ECHO_FEEDBACK, 20);
+                system->playSound(sound, NULL, false, &channel);
+                channel->addDSP(0, pDSP);
+                break;
+            default:
+
+                break;
+        }
+
+        system->update();
+        bool isPlaying = true;
+        // 需要等待，等声音全部播放完成
+        // 检查是否播放完成
+        while (isPlaying) {
+            channel->isPlaying(&isPlaying);
+            usleep(1000);
+        }
+
+    } catch (...) {
+        LOGE("%s", "error");
+        goto end;
+    }
+
+    // 回收内存
+    end:
+    system->close();
+    system->release();
+    jniEnv->ReleaseStringUTFChars(jstring1, path);
+
 }
